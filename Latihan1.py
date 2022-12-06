3dc2356956366f010c2a0ba286d23a3d5bbe3790
Rhendy Diki N <rendydikinugraha@gmail.com>
2022-12-06 20:11:50 +0700

    File Python

--- Latihan1.py
+++ Latihan1.py
@@ -1,26 +1,26 @@
-#FUNGSI LAMBDA
+#FUNGSI LAMBDA (Rhendy Diki Nugraha)
 import math
 
 def a(x):
     return x ** 2
 ac = lambda x : x ** 2
-print(a(8))
-print(ac(11))
+print(a(4))
+print(ac(10))
 
 def b(x, y):
     return math.sqrt(x**2 + y**2)
 bc = lambda x, y :math.sqrt(x**2 + y**2)
-print(b(1,4))
-print(bc(2,5))
+print(b(2,4))
+print(bc(6,9))
 
 def c(*args):
     return sum(args)/len(args)
 cc = lambda*args: sum (args)/len(args)
-print(c(100,50))
-print(cc(100,25))
+print(c(20,75))
+print(cc(55,90))
 
 def d(s):
     return "".join(set(s))
 dc = lambda s :"".join(set(s))
-print(d("ASU"))
-print(dc("Hallo"))
+print(d("Hallo"))
+print(dc("Hai"))
--- Praktikum.py
+++ Praktikum.py
@@ -1,30 +1,31 @@
+# Rhendy Diki Nugraha
 from os import system
-s_nama = []
-s_nim = []
-s_tugas = []
-s_uts = []
-s_uas = []
-s_akhir = []
+nama_m = []
+nim_m = []
+n_tugas = []
+n_uts = []
+n_uas = []
+n_akhir = []
 
 
 def judul():
-    print('==================================')
+    print('=================================')
     print('|     Daftar Nilai Mahasiswa     |')
     print('==================================')
 
 
 def menu():
     system('cls')
-    print('=====================================')
+    print('====================================')
     print('Input Data Nilai Mahasiswa'.center(40))
     print('=====================================')
     print('|    1. Tambah Data                 |')
     print('|    2. Tampilkan Data Mahasiswa    |')
     print('|    3. Ubah Data Mahasiswa         |')
     print('|    4. Hapus Data Mahasiswa        |')
-    print('|    5. Selesai                     |')
+    print('|    5. Keluar                      |')
     print('=====================================')
-    choose = input('Pilih Menu  : ')
+    choose = input('Pilih Menu diatas : ')
     if choose == '1':
         tambah()
     elif choose == '2':
@@ -34,40 +35,32 @@ def menu():
     elif choose == '4':
         hapus()
     elif choose == '5':
-        selesai()
+        keluar()
     else:
-        tidak = input('Menu Tidak Ada')
-        system('cls')
+        input('Menu Tidak Ada')
+
         menu()
 
 
 def tambah():
     system('cls')
     judul()
-    print('Tambah Data'.center(40))
+    print('\nTambah Data')
     print('==================================')
     nama = input('Nama     : ')
-    s_nama.append(nama)
-    nim = input('NIM       : ')
-    s_nim.append(nim)
-
-    system('cls')
-    judul()
-    print('Tambah Data'.center(40))
-    print('==================================')
-    tugas = float(input('Nilai Tugas        : '))
-    s_tugas.append(tugas)
-
-    uts = float(input('Nilai UTS        : '))
-    s_uts.append(uts)
-
-    uas = float(input('Nilai UAS        : '))
-    s_uas.append(uas)
-
-    total = tugas * 0.30 + uts * 0.35 + uas * 0.35
-    s_akhir.append(total)
-    print('Data Tersimpan'.center(40))
-    kembali = input('Kembali [Enter]')
+    nama_m.append(nama)
+    nim = input('NIM      : ')
+    nim_m.append(nim)
+    tugas = int(input('Nilai Tugas      : '))
+    n_tugas.append(tugas)
+    uts = int(input('Nilai UTS        : '))
+    n_uts.append(uts)
+    uas = int(input('Nilai UAS        : '))
+    n_uas.append(uas)
+    akhir = tugas * 0.30 + uts * 0.35 + uas * 0.35
+    n_akhir.append(akhir)
+    print('Data berhasil ditambah\n'.center(40))
+    input('Kembali ke menu [Enter]')
     menu()
 
 
@@ -75,58 +68,67 @@ def tampilkan():
     system('cls')
     judul()
 
-    for i in range(len(s_nim)):
-        print('%d. Nama         : %s' % (i + 0, s_nama[i]))
-        print('    NIM          : %s' % s_nim[i])
-        print('    Nilai Tugas  : %.2f' % s_tugas[i])
-        print('    UTS          : %.2f' % s_uts[i])
-        print('    UAS          : %.2f' % s_uas[i])
-        print('    Nilai Akhir  : %.2f' % s_akhir[i])
+    for i in range(len(nama_m)):
+        print('%d.\nNama         : %s' % (i + 0, nama_m[i]))
+        print('NIM          : %s' % nim_m[i])
+        print('Nilai Tugas  : %.d' % n_tugas[i])
+        print('UTS          : %.d' % n_uts[i])
+        print('UAS          : %.d' % n_uas[i])
+        print('Nilai Akhir  : %.2f' % n_akhir[i])
         print('-----------------------------')
-    kembali = input('Kembali Tekan [Enter]')
+    input('Kembali ke menu [Enter]')
     menu()
 
 
 def ubah():
-    rubah = input('Ubah Biodata Tekan [B]   : ')
-    if rubah == 'B' or rubah == 'b':
+    y_ubah = input('Yakin ubah data? [B]   : ')
+    if y_ubah == 'B' or y_ubah == 'b':
         i = int(input('Masukkan Nomor Urut  : '))
-        if (i > len(s_nim[i])):
+
+        if (i > len(nim_m[i])):
             print('Nomor Urut Salah')
         else:
-            namabaru = input('Nama      : ')
-            s_nama[i] = namabaru
-    kembali = input('Kembali Tekan [Enter]')
+            print('Masukkan data baru')
+            print('-----------------------------')
+            n_nama = input('Nama      : ')
+            nama_m[i] = n_nama
+            n_nim = input('NIM       : ')
+            nim_m[i] = n_nim
+            tugas_n = int(input('Nilai Tugas      :'))
+            n_tugas[i] = tugas_n
+            uts_n = int(input('Nilai UTS        :'))
+            n_uts[i] = uts_n
+            uas_n = int(input('Nilai UAS        :'))
+            n_uas[i] = uas_n
+    input('Kembali ke menu [Enter]')
     menu()
 
 
 def hapus():
     system('cls')
     judul()
-    print('Hapus Data'.center(40))
+    print('Hapus Data')
     print('=================================')
     i = int(input('Masukkan Nomor Urut  : '))
 
-    if (i > len(s_nim[i])):
-        tidak = input('NIM Tidak Ada')
-        hapus()
+    if (i > len(nama_m)):
+        input('Nama Tidak Ada [Enter]')
+        menu()
 
     else:
-        s_nim.remove(s_nim[i])
-        s_nama.remove(s_nama[i])
-        s_tugas.remove(s_tugas[i])
-        s_uts.remove(s_uts[i])
-        s_uas.remove(s_uas[i])
-        s_akhir.remove(s_akhir[i])
+        nama_m.remove(nama_m[i])
+        nim_m.remove(nim_m[i])
+        n_tugas.remove(n_tugas[i])
+        n_uts.remove(n_uts[i])
+        n_uas.remove(n_uas[i])
+        n_akhir.remove(n_akhir[i])
 
     print('Data Berhasil Di Hapus')
-    kembali = input('Kembali Tekan [Enter]')
+    input('Kembali ke menu [Enter]')
     menu()
 
 
-def selesai():
+def keluar():
     system('cls')
-
-
 
 menu()
